453175921ce0655d324708692b67750646ef59de
Adil ABBADI <adil.abbadi.1996@gmail.com>
2024-02-25 16:01:36 +0100

    remove uri

--- app.ts
+++ app.ts
@@ -6,29 +6,20 @@ console.log('hello worold');
 
 // Main function
 async function main() {
-  
-    await connectToDatabase("mongodb+srv://adilabbadi1996:xfClbv4jzWQLkmis@cluster0.thnqkdd.mongodb.net/First");
-    await insertJSONData("bravos");
-    await disconnectDatabase();
 
-
-
-
- 
-
-    // const rl = readline.createInterface({
-    //     input: process.stdin,
-    //     output: process.stdout
-    // });
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
     
-    // rl.question('MongoDb connection string :',async (answer) => {
-    //     await connectToDatabase(answer);
-    //     rl.question('Collection Name :',async (answer) => {
-    //         await insertJSONData(answer);
-    //         await disconnectDatabase();
-    //         rl.close();
-    //     });
-    // });
+    rl.question('MongoDb connection string :',async (answer) => {
+        await connectToDatabase(answer);
+        rl.question('Collection Name :',async (answer) => {
+            await insertJSONData(answer);
+            await disconnectDatabase();
+            rl.close();
+        });
+    });
    
       
 }
