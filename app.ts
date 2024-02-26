b16b2192f98193d7a222e937b064c106ebf83f65
Adil ABBADI <adil.abbadi.1996@gmail.com>
2024-02-26 13:45:50 +0100

    id: generate on empty

--- app.ts
+++ app.ts
@@ -1,27 +1,28 @@
+import mongoose from "mongoose";
 import { connectToDatabase, disconnectDatabase, insertJSONData } from "./mongodb/mongodb";
 import * as readline from 'readline';
 
 
-console.log('hello worold');
-
 // Main function
 async function main() {
+
+
 
     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout
     });
-    
-    rl.question('MongoDb connection string :',async (answer) => {
+
+    rl.question('MongoDb connection string :', async (answer) => {
         await connectToDatabase(answer);
-        rl.question('Collection Name :',async (answer) => {
+        rl.question('Collection Name :', async (answer) => {
             await insertJSONData(answer);
             await disconnectDatabase();
             rl.close();
         });
     });
-   
-      
+
+
 }
 
 // Execute main function
--- mongodb/mongodb.ts
+++ mongodb/mongodb.ts
@@ -3,11 +3,11 @@
 
 import mongoose, { Document, Schema, Model } from 'mongoose';
 import * as fs from 'fs'
-import lastSchema from './schema';
-import { BSON } from 'mongodb';
+import collectionSchema from './schema';
 
 
-export async function connectToDatabase(connectionString : string) {
+
+export async function connectToDatabase(connectionString: string) {
     try {
         await mongoose.connect(connectionString);
         console.log('Connected to MongoDB');
@@ -19,29 +19,32 @@ export async function connectToDatabase(connectionString : string) {
 
 
 
-export async function insertJSONData(collectionName : string = 'test') {
+export async function insertJSONData(collectionName: string = 'test') {
 
     try {
-        const collection =  mongoose.model(collectionName, lastSchema)
+        const collection = mongoose.model(collectionName, collectionSchema)
         // Read JSON file
         const jsonStr = await fs.readFileSync('./data/jsonArray.json', 'utf-8');
         const jsonData: any[] = JSON.parse(jsonStr);
 
 
         //  // Replace MongoDB-specific data types
-         const processedData = correctData(jsonData)
+        const processedData = correctData(jsonData)
 
         // Insert JSON data into database
-        const promises =  processedData.map(async (element : any) => {
-            const { _id, ...rest } = element;   
-            const result = await collection.updateOne({_id}, { $set: rest }, { upsert: true});
+        const promises = processedData.map(async (element: any) => {
+            let { _id, ...rest } = element;
+            if (_id == null || _id == undefined) {
+                _id = new mongoose.Types.ObjectId()
+            }
+            const result = await collection.updateOne({ _id }, { $set: rest }, { upsert: true });
             console.log(result);
-            
+
         });
 
         await Promise.all(promises);
 
-          
+
         console.log('Inserted JSON data into database');
     } catch (error) {
         console.error('Error inserting JSON data into database:', error);
@@ -56,38 +59,44 @@ export async function disconnectDatabase() {
 }
 
 
-function correctData(jsonData : any){
- // Recursively process each item in the JSON data
- const processItem : any = (item : any) => {
-    // Convert $numberInt to Number
-    if (item.hasOwnProperty('$numberInt')) {
-        return parseInt(item['$numberInt']);
-    }
-    // Convert $date to Date
-    if (item.hasOwnProperty('$date')) {
-       return new Date(parseInt(item['$date']["$numberLong"]));
-         
-    }
-    // Convert $oid to ObjectId
-    if (item.hasOwnProperty('$oid')) {
-        return new mongoose.Types.ObjectId(item['$oid']);
-    }
-    // If item is a list, recursively process each element in the list
-    if (Array.isArray(item)) {
-        return item.map(processItem);
-    }
-    // If item is an object, recursively process each value in the object
-    if (typeof item === 'object' && item !== null) {
-        let processedObject : any = {};
-        for (const key in item) {
-            processedObject[key] = processItem(item[key]);
+function correctData(jsonData: any) {
+    // Recursively process each item in the JSON data
+    const processItem: any = (item: any) => {
+        // Convert $numberInt to Number
+        if (item.hasOwnProperty('$numberInt')) {
+            return parseInt(item['$numberInt']);
         }
-        return processedObject;
-    }
-    // Otherwise, return the item unchanged
-    return item;
-};
+        // Convert $date to Date
+        if (item.hasOwnProperty('$date')) {
+            return new Date(parseInt(item['$date']["$numberLong"]));
 
-// Process each item in the JSON data
-return jsonData.map(processItem);
+        }
+        // Convert $oid to ObjectId
+        if (item.hasOwnProperty('$oid')) {
+            if (!item['$oid']) {
+                return new mongoose.Types.ObjectId()
+            }
+            else {
+                return new mongoose.Types.ObjectId(item['$oid']);
+            }
+
+        }
+        // If item is a list, recursively process each element in the list
+        if (Array.isArray(item)) {
+            return item.map(processItem);
+        }
+        // If item is an object, recursively process each value in the object
+        if (typeof item === 'object' && item !== null) {
+            let processedObject: any = {};
+            for (const key in item) {
+                processedObject[key] = processItem(item[key]);
+            }
+            return processedObject;
+        }
+        // Otherwise, return the item unchanged
+        return item;
+    };
+
+    // Process each item in the JSON data
+    return jsonData.map(processItem);
 }
